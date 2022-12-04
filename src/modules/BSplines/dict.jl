e86022d7da1e3cc351d3bf075f718f6007ef62df
Daan Huybrechs <daan.huybrechs@kuleuven.be>
2022-12-04 10:47:12 +0100

    simplify periodic splines

--- src/modules/BSplines/dict.jl
+++ src/modules/BSplines/dict.jl
@@ -31,6 +31,7 @@ centers(Φ::RegularBSplines) = nodes(Φ)
 kernel(Φ::RegularBSplines{T}) where {T} =
     CenteredBSpline{T}(Φ.degree)
 
+# Implement a linear scaling
 function map_to_kernel(Φ::RegularBSplines, i)
     h = step(Φ)
     c = center(Φ, i)
@@ -56,29 +57,4 @@ Base.size(Φ::PeriodicBSplines) = (Φ.n,)
 spline_degree(Φ::PeriodicBSplines) = Φ.degree
 
 parent_kernel(Φ::PeriodicBSplines{T}) where {T} =
-    BSpline{T}(Φ.degree)
-
-# We use as a kernel the BSpline on [0,degree+1]
-kernel_support(Φ::PeriodicBSplines{T}) where {T} =
-    Interval(zero(T), spline_order(Φ))
-
-# The basis functions are centered (and dilated) splines.
-# Exception for even degree splines: we align the origin with a node,
-# rather than the origin being in between two nodes. This allows for
-# exact conversion between spline spaces of different degree.
-semicentered_shift(degree::Int, ::Type{T}) where {T} =
-    iseven(degree) ? T(degree)/2 : T(degree+1)/2
-semicentered_shift(Φ::PeriodicBSplines{T}) where {T} =
-    semicentered_shift(spline_degree(Φ), T)
-
-# the map from [0,1] to the kernel domain takes into account the
-# shift by i, the shift of the kernel, and a scaling by N
-map_to_kernel(Φ::PeriodicBSplines, i) =
-    AffineMap(length(Φ), -(i-1) + semicentered_shift(Φ))
-
-# this version is slightly more accurate than the default
-function BasisFunctions.support(Φ::PeriodicBSplines, idx)
-    shift = semicentered_shift(Φ)
-    subdomain = Interval(-shift+(idx-1), -shift+(idx-1)+spline_order(Φ))/length(Φ)
-    PeriodicInterval(subdomain, support(Φ))
-end
+    CenteredBSpline{T}(Φ.degree)
